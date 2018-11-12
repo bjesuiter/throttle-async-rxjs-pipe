a9f5336d6deab9b65ddd2dd92547c78ca46015a7
Benjamin Jesuiter <bjesuiter@gmail.com>
2018-11-12 23:39:42 +0100

    testing-setup: add test for: Should emit 2 elements after 3 seconds, when waitUntil$ does emit one event after 1500 ms

--- src/lib/throttle-async.spec.ts
+++ src/lib/throttle-async.spec.ts
@@ -1,16 +1,15 @@
 import {EMPTY, interval, of, TimeoutError} from "rxjs";
-import {catchError, timeout} from "rxjs/operators";
+import {catchError, take, timeout} from "rxjs/operators";
 // import {throttleAsync} from "throttle-async-lib/throttle-async";
 import {throttleAsync} from "./throttle-async";
 
-describe('Throttle Async Pipe', () => {
+describe('Throttle Async Pipe (Source Interval 500)', () => {
     describe('Without any waitUntil$ emission', () => {
-
         it('Should emit first element', (done) => {
             const waitObservable = EMPTY;
             let eventCount = 0;
 
-            interval(500).pipe(
+            const subscription = interval(500).pipe(
                 throttleAsync(waitObservable),
                 timeout(1500),
                 catchError((error) => {
@@ -20,11 +19,34 @@ describe('Throttle Async Pipe', () => {
             ).subscribe((data) => {
                 if (data === 'timeout') {
                     expect(eventCount === 1);
+                    subscription.unsubscribe();
+                    done()
+                }
+            });
+        });
+    });
+
+    describe('With one waitUntil$ emission after 2 sek', function () {
+        it('Should have emitted two elements after 3 sek', (done) => {
+
+            let eventCount = 0;
+            const waitUntil$ = interval(1500).pipe(take(1));
+
+            const subscription = interval(500).pipe(
+                throttleAsync(waitUntil$),
+                timeout(3000),
+                catchError((error) => {
+                    expect(error).toBeInstanceOf(TimeoutError);
+                    return of('timeout')
+                })
+            ).subscribe((data) => {
+                if (data === 'timeout') {
+                    expect(eventCount === 2);
+                    subscription.unsubscribe();
                     done()
                 }
             });
         })
-
-    })
+    });
 
 });
