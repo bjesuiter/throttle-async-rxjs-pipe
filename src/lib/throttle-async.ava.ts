09d9de411819fcc73c887d995c8ae534ff90ea26
Benjamin Jesuiter <benjamin.jesuiter@flavia-it.de>
2020-11-01 15:27:21 +0100

    add second ava test

--- src/lib/throttle-async.ava.ts
+++ src/lib/throttle-async.ava.ts
@@ -10,8 +10,8 @@ import {throttleAsync} from './throttle-async';
  * Should emit first element and wait 'indefinitely'
  * (test waits for 3000 ms)
  */
-test(`ThrottleAsync Pipe should emit first event`, t => {
-	const waitObservable = EMPTY;
+test.serial(`ThrottleAsync Pipe should emit first event`, t => {
+	const waitUntil$ = EMPTY;
 	let eventCount = 0;
 	let absoluteTime = 0;
 
@@ -20,16 +20,50 @@ test(`ThrottleAsync Pipe should emit first event`, t => {
 			absoluteTime += 500;
 			console.log(`Time Passed: ${absoluteTime}`);
 		}),
-		throttleAsync(waitObservable),
+		throttleAsync(waitUntil$),
 		tap(() => {
+			eventCount++;
 			console.log(`Emit after ThrottleAsync: ${absoluteTime}`);
 		}),
-		map(() => eventCount++),
 		timeout(2000),
 		catchError(error => {
-			console.log(`TimeoutEvent: `, error);
+			console.log(`TimeoutEvent: ${error.message}`);
 			t.true(error instanceof TimeoutError);
 			t.is(eventCount, 1);
+			clear;
+			return of('timeout');
+		})
+	);
+});
+
+/**
+ * Throttle Async Pipe (Source Interval 500)
+ * With one waitUntil$ emission after 2 sek
+ * Should have emitted two elements after 3 sek
+ */
+test.serial(`ThrottelAsync Pipe should emit next event after waitObservable$ emitted`, t => {
+	const waitUntil$ = interval(2000).pipe(
+		take(1),
+		tap(() => console.log('Observable waitUntil$ emitted!'))
+	);
+	let eventCount = 0;
+	let absoluteTime = 0;
+
+	return interval(500).pipe(
+		tap(() => {
+			absoluteTime += 500;
+			console.log(`Time Passed: ${absoluteTime}`);
+		}),
+		throttleAsync(waitUntil$),
+		tap(() => {
+			eventCount++;
+			console.log(`Emit after ThrottleAsync: ${absoluteTime}`);
+		}),
+		timeout(3000),
+		catchError(error => {
+			console.log(`TimeoutEvent: ${error.message}`);
+			t.true(error instanceof TimeoutError);
+			t.is(eventCount, 2);
 			clear;
 			return of('timeout');
 		})
