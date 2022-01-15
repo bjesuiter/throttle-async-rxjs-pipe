ed96132c10ca4800574f5de97273e03b1bbc3cad
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-01-15 23:38:57 +0100

    add config file for web-test-runner

---
+++ web-test-runner.config.js
@@ -0,0 +1,9 @@
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+
+export default {
+	files: [
+		'src/**/*.test.ts',
+		// 'src/**/*.spec.ts'
+	],
+	plugins: [esbuildPlugin({ ts: true })],
+};
